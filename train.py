a2e5ffb82ae6e78e8b9ae905e872ff726fba290b
kmftcy <kmftangchaoyang@gmail.com>
2021-12-19 12:44:12 +0800

    add new f1 score

--- train.py
+++ train.py
@@ -65,6 +65,40 @@ classes = [
 ]
 
 
+def create_f1():
+    def f1_function(y_true, y_pred):
+        y_pred_binary = tf.where(y_pred >= 0.5, 1., 0.)
+        tp = tf.reduce_sum(y_true * y_pred_binary)
+        predicted_positives = tf.reduce_sum(y_pred_binary)
+        possible_positives = tf.reduce_sum(y_true)
+        return tp, predicted_positives, possible_positives
+    return f1_function
+
+
+class F1_score(keras.metrics.Metric):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)  # handles base args (e.g., dtype)
+        self.f1_function = create_f1()
+        self.tp_count = self.add_weight("tp_count", initializer="zeros")
+        self.all_predicted_positives = self.add_weight(
+            'all_predicted_positives', initializer='zeros')
+        self.all_possible_positives = self.add_weight(
+            'all_possible_positives', initializer='zeros')
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        tp, predicted_positives, possible_positives = self.f1_function(
+            y_true, y_pred)
+        self.tp_count.assign_add(tp)
+        self.all_predicted_positives.assign_add(predicted_positives)
+        self.all_possible_positives.assign_add(possible_positives)
+
+    def result(self):
+        precision = self.tp_count / self.all_predicted_positives
+        recall = self.tp_count / self.all_possible_positives
+        f1 = 2*(precision*recall)/(precision+recall)
+        return f1
+
+
 # read dataset
 target_rows = 1000000
 f = open("train.txt", encoding="utf-8")
@@ -124,6 +158,8 @@ tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
 args = hyperparameters.args
 model = BERT_model.create_model(
     args['bert_model_name'], args['label_num'])
+model.compile(loss='binary_crossentropy',
+              optimizer="adam", metrics=[F1_score()])
 model.fit(train_x, train_y, epochs=args["epoch"], verbose=1,
           callbacks=[Metrics(valid_data=(val_x, val_y)), tensorboard_callback],
           batch_size=args["batch_size"],
